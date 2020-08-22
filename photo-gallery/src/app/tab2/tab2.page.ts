bcc7095566c2377fc36353dee7d8f7d188cec66b
claudiavieira <claudia.araujo16@gmail.com>
2020-08-22 16:49:44 -0300

    first-commit

---
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+
+export class Tab2Page {
+  photos = this.photoService.photos;
+
+  constructor(public photoService: PhotoService) {}
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+  
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+}
